78b8ce8c3450f08d128427d8ed971cd5b84ab1a6
wafam <wafa.mohbubul@hotmail.com>
2023-10-20 10:49:53 +0100

    updates

---
+++ variable.tf
@@ -0,0 +1,10 @@
+# variable.tf - create
+# create a var for aws region
+variable "aws-region"{
+    default = "eu-west-1"
+}
+
+variable "web-app_ami_id" {
+    default = "ami-0943382e114f188e8"
+}
+
